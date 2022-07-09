cd489c52468ae216fc1b05c86862e05827cc232d
BobbySU <radio_20bg@yahoo.com>
2022-07-09 16:26:12 +0300

    Add Bus Stop

--- Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    console.log("TODO...");
+    let urlBase = `http://localhost:3030/jsonstore/bus/businfo/`;
+    let input = document.getElementById('stopId');
+    let divEl = document.getElementById('stopName');
+    let ulEl = document.getElementById('buses');
+
+    divEl.textContent = '';
+    ulEl.innerHTML = '';
+
+    fetch(`${urlBase}${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            let busList = data.buses;
+            let name = data.name;
+            divEl.textContent = name;
+            Object.entries(busList).forEach(([busId, time]) => {
+                let li = document.createElement('li');
+                li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                ulEl.appendChild(li);
+            })
+        })
+        .catch(() => {
+            stopName.textContent = 'Error';
+        });
 }
