cf79016a64a6c5c8c7b4daa7a62941738ec93332
BobbySU <radio_20bg@yahoo.com>
2022-07-10 01:52:05 +0300

    Add Blog

--- Asynchronous Programming - Exercise/06.Blog/app.js
+++ Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,62 +1,62 @@
 function attachEvents() {
-  let postsSelect = document.querySelector('select#posts');
-  let btnLoadPosts = document.getElementById('btnLoadPosts');
-  let btnViewPost = document.getElementById('btnViewPost');
-  let postTitle = document.getElementById('post-title');
-  let postContent = document.getElementById('post-body');
+    let postsSelect = document.querySelector('select#posts');
+    let btnLoadPosts = document.getElementById('btnLoadPosts');
+    let btnViewPost = document.getElementById('btnViewPost');
+    let postTitle = document.getElementById('post-title');
+    let postContent = document.getElementById('post-body');
 
-  btnLoadPosts.addEventListener('click', handleLoadPosts);
-  btnViewPost.addEventListener('click', handleViewPost);
-  let commonData;
+    btnLoadPosts.addEventListener('click', handleLoadPosts);
+    btnViewPost.addEventListener('click', handleViewPost);
+    let commonData;
 
-  function handleLoadPosts() {
-      fetch('http://localhost:3030/jsonstore/blog/posts')
-      .then(res => res.json())
-      .then(data => addPosts(data));
+    function handleLoadPosts() {
+        fetch('http://localhost:3030/jsonstore/blog/posts')
+            .then(res => res.json())
+            .then(data => addPosts(data));
 
-      function addPosts(data) {
-          commonData = data;
+        function addPosts(data) {
+            commonData = data;
 
-          postsSelect.innerHTML = '';
-          
-          for (let [id, postInfo] of Object.entries(data)) {
-              let option = document.createElement('option');
-              option.value = id;
-              option.textContent = postInfo.title;
-              option.dataset.body = postInfo.body;
-              postsSelect.appendChild(option);
-          }
-      }
-  }
+            postsSelect.innerHTML = '';
 
-  function handleViewPost() {
-      let selectedPostId = document.getElementById('posts').value;
+            for (let [id, postInfo] of Object.entries(data)) {
+                let option = document.createElement('option');
+                option.value = id;
+                option.textContent = postInfo.title;
+                option.dataset.body = postInfo.body;
+                postsSelect.appendChild(option);
+            }
+        }
+    }
 
-      postTitle.textContent = commonData[selectedPostId].title;
-      postContent.textContent = commonData[selectedPostId].body;
+    function handleViewPost() {
+        let selectedPostId = document.getElementById('posts').value;
 
-      fetch('http://localhost:3030/jsonstore/blog/comments')
-      .then(res => res.json())
-      .then(data => handleComments(data));
+        postTitle.textContent = commonData[selectedPostId].title;
+        postContent.textContent = commonData[selectedPostId].body;
 
-      function handleComments(data) {
-          let commentsUl = document.getElementById('post-comments');
-          commentsUl.innerHTML = '';
+        fetch('http://localhost:3030/jsonstore/blog/comments')
+            .then(res => res.json())
+            .then(data => handleComments(data));
 
-          Object.entries(data).forEach(commentInfo =>
-        //   for (let commentInfo of Object.entries(data)) 
-          {
-              if (commentInfo[1].postId == selectedPostId) {
-                  let li = document.createElement('li');
-                  li.id = commentInfo[1].id
-                  li.textContent = commentInfo[1].text;
-                  commentsUl.appendChild(li);
-              }
-              
-          }
-          )
-      }
-  }
+        function handleComments(data) {
+            let commentsUl = document.getElementById('post-comments');
+            commentsUl.innerHTML = '';
+
+            Object.entries(data).forEach(commentInfo =>
+                    //   for (let commentInfo of Object.entries(data))
+                {
+                    if (commentInfo[1].postId == selectedPostId) {
+                        let li = document.createElement('li');
+                        li.id = commentInfo[1].id
+                        li.textContent = commentInfo[1].text;
+                        commentsUl.appendChild(li);
+                    }
+
+                }
+            )
+        }
+    }
 }
 
 attachEvents();
