616977d1c37ed65c15570182445a321fd44406a2
BobbySU <radio_20bg@yahoo.com>
2022-07-09 18:22:31 +0300

    Add Bus Schedule

--- Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,33 @@
 function solve() {
+    let urlBase = `http://localhost:3030/jsonstore/bus/schedule/`;
+    let info = document.querySelector('span.info');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    let nextStop = 'depot';
+    let stopName = '';
 
     function depart() {
-        console.log('Depart TODO...');
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+        fetch(`${urlBase}${nextStop}`)
+            .then(response => response.json())
+            .then(data => {
+                nextStop = data.next;
+                stopName = data.name;
+                info.textContent = `Next stop ${stopName}`;
+            })
+            .catch(() => {
+                info.textContent = 'Error';
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+        info.textContent = `Arriving at ${stopName}`;
     }
 
     return {
